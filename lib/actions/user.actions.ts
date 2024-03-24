b1523fb6bc522fb1c5914dd431b9bf8926ce387d
Mohd Aqib <37264147+Mohdaqib18@users.noreply.github.com>
2024-03-24 23:38:11 +0530

    reply feature added

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,25 +3,25 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
 
-interface Params{
-	userId:string,
-	username:string,
-	name:string,
-	bio:string,
-	image:string,
-	path:string
+interface Params {
+	userId: string;
+	username: string;
+	name: string;
+	bio: string;
+	image: string;
+	path: string;
 }
 
-export async function updateUser(
-{	userId,
+export async function updateUser({
+	userId,
 	username,
 	name,
 	bio,
 	image,
-	path
-}: Params
-): Promise<void> {
+	path,
+}: Params): Promise<void> {
 	connectToDB();
 	try {
 		await User.findOneAndUpdate(
@@ -46,16 +46,42 @@ export async function updateUser(
 	}
 }
 
-export async function fetchUser(userId: string){
-	try{
+export async function fetchUser(userId: string) {
+	try {
 		connectToDB();
-		return await User
-		.findOne({id:userId})
+		return await User.findOne({ id: userId });
 		// .populate({
 		// 	path:"communities",
 		// 	model:Community
 		// })
-	} catch(error:any){
-		throw new Error(`Failed to fetch user: ${error.message}`)
+	} catch (error: any) {
+		throw new Error(`Failed to fetch user: ${error.message}`);
+	}
+}
+
+export async function fetchUserPosts(userId: string) {
+	try {
+		connectToDB();
+
+		//Find all threads authored by user with the given user id
+
+		//TODO: populate community
+		const threads = await User.findOne({ id: userId }).populate({
+			path: "threads",
+			model: Thread,
+			populate: {
+				path: "children",
+				model: Thread,
+				populate: {
+					path: "author",
+					model: User,
+					select: "name image id",
+				},
+			},
+		});
+
+		return threads;
+	} catch (error: any) {
+		throw new Error(`Failed to fetch user posts: ${error.message}`);
 	}
 }
